ab9793d660b906391f57a078a96d2147fd6b705a
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-09-14 16:47:35 +0100

    making progress?

--- Package.swift
+++ Package.swift
@@ -6,10 +6,14 @@ import PackageDescription
 let package = Package(
     name: "swiftborders",
     platforms: [.macOS(.v15)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "swiftborders"),
+            name: "swiftborders", dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+        )
     ]
 )
--- Sources/borders.swift
+++ Sources/borders.swift
@@ -1,16 +1,11 @@
 import SwiftUI
 import Foundation
+import ArgumentParser
 
 // need to take cli args to set border colours etc
 
 // if window count is not 0
 // first get dimensions of window(s), then pass to rectangle dimensions
-
-// active vs inactive colour
-@MainActor 
-let testList = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
-
-
 @MainActor
 func quit() {
     NSApp.terminate(nil)
@@ -26,19 +21,40 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     }
 }
 
+@MainActor
+func getWindowList() -> CFArray? {
+    let windowList = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
+    return windowList
+}
+
+/*
+// need some sort of listener/callback for window changes (new/delete/move/resize).
+// consider multiple functions
+func updateWindowList() {
+
+}
+*/
+
+@MainActor
+var windowList = getWindowList()
+// CGWindowBounds: Height, Width, X, Y
 // do for each loop over list for rects
+// active vs inactive colour
 @main
 struct swiftborders: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    var winCount = Int(CFArrayGetCount(testList))
+    var winCount = Int(CFArrayGetCount(windowList))
     var body: some Scene {
         WindowGroup {
-            Rectangle()
-                .background(.clear)
-                .border(.white)
-                .shadow(radius: 10)
-                .frame(maxWidth: 240)
-                .frame(maxHeight: 150)
+            ForEach(0..<winCount) { window in
+                //if let winProperties = CFArrayGetValueAtIndex(windowList, window) {
+                Rectangle()
+                    .frame(maxWidth: 240)
+                    .frame(maxHeight: 150)
+                    .background(.clear)
+                    .border(.white)
+                    .shadow(radius: 10)
+            }
         }
         .windowStyle(.plain)
         .windowResizability(.contentSize)
