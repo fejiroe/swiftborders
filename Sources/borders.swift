227c5d49e3cdaf4947d8c725b1280bc079c1a4bf
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-09-20 11:47:19 +0100

    how to access dictionary values

--- Sources/borders.swift
+++ Sources/borders.swift
@@ -2,76 +2,50 @@ import SwiftUI
 import Foundation
 import ArgumentParser
 
-// need to take cli args to set border colours etc
-
-// if window count is not 0
-// first get dimensions of window(s), then pass to rectangle dimensions
-@MainActor
-func quit() {
+@MainActor func quit() {
     NSApp.terminate(nil)
 }
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         if let window = NSApplication.shared.windows.first {
-           window.titlebarAppearsTransparent = true
-           window.isOpaque = false
-           window.backgroundColor = NSColor.clear
+            window.titlebarAppearsTransparent = true
+            window.isOpaque = false
+            window.backgroundColor = NSColor.clear
         }
     }
 }
 
-@MainActor
-// func getWindowList() {
-func getWindowList() -> CFArray? {
-    let windowsCFArray = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
-    return windowList
-}
+func testFunc() {
+    // switch to AX or NS window approach instead?
+    func getWinArray() -> CFArray? {
+        let windowsCFArray = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
+            return windowsCFArray
+    }
+    let winArray: CFArray? = getWinArray()
+    let winCount = Int(CFArrayGetCount(winArray))
 
-func setColour() {
-    // ideally take argument for active/inactive
-}
+    func toRect() {
+            for window in 0...winCount {
+                // let dictRef = CFArrayGetValueAtIndex(winArray, window) as CFDictionary?
+                // if let dict = dictRef as? [String: AnyObject] {
+                    // print(dict["kCGWindowBounds"]}
+                // CFDictionaryGetValue(dictRef, kCGWindowBounds)
+                // var winRect: UnsafeMutablePointer<CGRect>
+                // CGRectMakeWithDictionaryRepresentation(winProperties, winRect)
+            }
+    }
 
-// /*
-// need some sort of listener/callback for window changes (new/delete/move/resize).
-// consider multiple functions. updateWinCount() ?
-@MainActor
-func updateWinList() {
-    windowList = getWindowList()
-}
-// */
-
-@MainActor
-var windowList = getWindowList()
-// CGWindowBounds: Height, Width, X, Y
-// do for each loop over list for rects
-// active vs inactive colour
-@MainActor
-func printWindowInfo() -> Void {
-    var winCount = Int(CFArrayGetCount(windowList))
-    // var winDicts: [Any]
-    // var currentWindowProperties: Any
-    for window in 0...winCount {
-       // let input = CFArrayGetValueAtIndex(windowList, window)
-       // winDicts.append(input)
-       // let output = winDicts[window]
-       // print(input)
-       // currentWindowProperties = input
-       // NSLog("%@", CFDictionaryGetValue(input, kCGWindowBounds))
-       // NSLog("%@", input)
-       // var test = CFDictionaryGetValue(kCGWindowBounds, input)
-       // let ref: CFDictionary = CFArrayGetValueAtIndex(windowList, window)
+    func printWin() {
     }
 }
 
-@main
-struct swiftborders: App {
+@main struct swiftborders: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    // var winCount = Int(CFArrayGetCount(windowList))
+        // var windowList: CFArray?
     var body: some Scene {
         WindowGroup {
             // ForEach(0..<winCount) { window in windowList // find way to cast array ?
-                // if let winProperties = CFArrayGetValueAtIndex(windowList, window) {
                 Rectangle()
                     .frame(maxWidth: 240)
                     .frame(maxHeight: 150)
@@ -79,10 +53,9 @@ struct swiftborders: App {
                     .border(.white)
                     .shadow(radius: 10)
                     .onAppear {
-                        updateWinList()
-                        printWindowInfo()
+                        testFunc()
                     }
-            // }
+                // }
         }
         .windowStyle(.plain)
         .windowResizability(.contentSize)
