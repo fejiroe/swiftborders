ffc1f74ed688d44df79890aec8128143a61cbb30
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-09-23 16:03:17 +0100

    error why?

--- Sources/windows.swift
+++ Sources/windows.swift
@@ -1,6 +1,13 @@
 import Foundation
 import SwiftUI
 
+/*
+https://github.com/tornikegomareli/space-borders
+https://stackoverflow.com/questions/25575195/how-to-use-axuielementcopyattributevalue-from-swift
+https://stackoverflow.com/questions/49285656/swift-4-axvaluegetvalue-optional-value-is-nil-but-debugger-shows-a-value
+
+*/
+
 // struct WindowManager {
 struct WindowInfo {
     var pid: String
@@ -9,6 +16,7 @@ struct WindowInfo {
     var xySize: CGSize
     var isFocused: Bool = false
 }
+
 func getWinArray() -> CFArray? {
         let winCFArray = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
         return winCFArray
@@ -27,35 +35,35 @@ func test() {
             var winInfo: WindowInfo
 
             var posRef: UnsafeMutablePointer<CFTypeRef?>
-            var posRefVal: CFTypeRef?
-            var posVal: AXValue
+            var posVal: AnyObject?
             var pos: CGPoint?
-            /*
-            posRef.initialize(to: posRefVal)
-            
-            // docs: Returns the value of an accessibility object's attribute.
-            let copyPosSuccess = AXUIElementCopyAttributeValue(winElement, kAXPositionAttribute as CFString, posRef)
-            if copyPosSuccess == AXError.success { // need to cover error cases
+            let copyPos = AXUIElementCopyAttributeValue(winElement, kAXPositionAttribute as CFString, &posVal)
+            if copyPos == AXError.success { // need to cover error cases
+                // print(posVal as! AXUIElement)
+                print(posVal)
             /*
             docs:
             Decodes the structure stored in value and copies it into valuePtr.
             If the structure stored in value is not the same as requested by theType,
             the function returns false.
             */
-            // AXValueGetValue(posRef, kAXValueCGPointType, pos)
-            AXValueGetValue(posVal, AXValueType.cgPoint, posRef)
+            // AXValueGetValue(posVal as! AXUIElement, AXValueType.cgPoint, posRef)
             // var winPos = AXValueGetValue(kAXPositionAttribute, positionVal, winElement)
             // winInfo.pid = ""
             // winInfo.position = AXValueGetValue(positionVal, kAXValueCGPointType, winInfo.position.self)
             // print(windowElement as Any)
-            }
-            */
+            } else {print(copyPos)}
 
-            var sizeRef: UnsafeMutablePointer<CFTypeRef>
-            var sizeVal: AXValue
+            var sizeVal: CFTypeRef?
             var size: CGSize
+            let copySize = AXUIElementCopyAttributeValue(winElement, kAXSizeAttribute as CFString, &sizeVal)
+            if copySize == AXError.success {
+                var sizeRef: UnsafeMutablePointer<CFTypeRef>
+                var val = CGSize.zero
+                // AXValueGetValue(sizeVal, AXValueType.cgSize, &val)
+            }
         }
     }
-    testFunc()
+    getWinInfo()
 }
 // }
