3b62ec65fcd5b2408bf45f4b57b4e0a66ef5163b
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-09-23 14:30:32 +0100

    still trying to get window position

--- Sources/windows.swift
+++ Sources/windows.swift
@@ -1,30 +1,61 @@
 import Foundation
 import SwiftUI
 
+// struct WindowManager {
 struct WindowInfo {
     var pid: String
-    var position: CGPoint
-    var dimensions: CGSize
+    var winRef: UnsafeRawPointer
+    var pos: CGPoint
+    var xySize: CGSize
     var isFocused: Bool = false
+}
+func getWinArray() -> CFArray? {
+        let winCFArray = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
+        return winCFArray
 }
 
 @MainActor
 func test() {
-    func getWinArray() -> CFArray? {
-        let windowsCFArray = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID)
-        return windowsCFArray
-    }
     let winArray: CFArray? = getWinArray()
     let winCount = Int(CFArrayGetCount(winArray))
 
-    func testFunc() {
+    func getWinInfo() {
         for window in 0...winCount {
             let ref = CFArrayGetValueAtIndex(winArray, window)
-            let windowElement: AXUIElement = unsafeBitCast(ref, to: AXUIElement.self)
-            // var winInfo: WindowInfo
-            // let key: String = "kCGWindowBounds"
-            print(windowElement as Any)
+            let winElement: AXUIElement = unsafeBitCast(ref, to: AXUIElement.self)
+
+            var winInfo: WindowInfo
+
+            var posRef: UnsafeMutablePointer<CFTypeRef?>
+            var posRefVal: CFTypeRef?
+            var posVal: AXValue
+            var pos: CGPoint?
+            /*
+            posRef.initialize(to: posRefVal)
+            
+            // docs: Returns the value of an accessibility object's attribute.
+            let copyPosSuccess = AXUIElementCopyAttributeValue(winElement, kAXPositionAttribute as CFString, posRef)
+            if copyPosSuccess == AXError.success { // need to cover error cases
+            /*
+            docs:
+            Decodes the structure stored in value and copies it into valuePtr.
+            If the structure stored in value is not the same as requested by theType,
+            the function returns false.
+            */
+            // AXValueGetValue(posRef, kAXValueCGPointType, pos)
+            AXValueGetValue(posVal, AXValueType.cgPoint, posRef)
+            // var winPos = AXValueGetValue(kAXPositionAttribute, positionVal, winElement)
+            // winInfo.pid = ""
+            // winInfo.position = AXValueGetValue(positionVal, kAXValueCGPointType, winInfo.position.self)
+            // print(windowElement as Any)
+            }
+            */
+
+            var sizeRef: UnsafeMutablePointer<CFTypeRef>
+            var sizeVal: AXValue
+            var size: CGSize
         }
     }
     testFunc()
 }
+// }
