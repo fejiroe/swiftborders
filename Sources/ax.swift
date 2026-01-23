425e9b348b8e709b2d5fb6b6c36595607a7cf1cd
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2026-01-23 10:00:12 +0000

    forgot to actually add

---
+++ Sources/ax.swift
@@ -0,0 +1,15 @@
+import ApplicationServices
+import Foundation
+
+func ensureAccessibilityPermission() {
+    let options =
+        [
+            "AXTrustetCheckOptionPrompt": true
+        ] as CFDictionary
+    let trusted = AXIsProcessTrustedWithOptions(options)
+    if !trusted {
+        print("accessibility permission not granted – user prompted.")
+    } else {
+        print("already trusted; can use AXUIElement APIs.")
+    }
+}
